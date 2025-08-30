6d8d0855d344a58e7d2ab127086944089a2c1722
Shaurya Mittal <136336857+shaurya174@users.noreply.github.com>
2025-08-30 20:20:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,9 @@ const port = process.env.PORT || 3000;
 // ========================
 
 const db = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+
   ssl: { rejectUnauthorized: false } // <-- Required for Supabase
 });
 
